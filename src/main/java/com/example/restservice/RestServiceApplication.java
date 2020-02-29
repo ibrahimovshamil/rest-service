1d582f4567e0af192de54016368f739a86d339e3
Yalchin Aliyev <yalchin.aliyev@outlook.com>
2020-03-01 03:42:01 +0400

    Database and image folder setup

--- src/main/java/com/example/restservice/RestServiceApplication.java
+++ src/main/java/com/example/restservice/RestServiceApplication.java
@@ -2,9 +2,22 @@ package com.example.restservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
-public class RestServiceApplication {
+public class RestServiceApplication implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        // Register resource handler for images
+        registry.addResourceHandler("/images/**").addResourceLocations("/WEB-INF/images/")
+                .setCacheControl(CacheControl.maxAge(2, TimeUnit.HOURS).cachePublic());
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(RestServiceApplication.class, args);
