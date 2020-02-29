7e5d3adb0cc3b0eee2e1e9d81b5cc2a1487ea4cb
Yalchin Aliyev <yalchin.aliyev@outlook.com>
2020-03-01 00:58:44 +0400

    Add Product model class

---
+++ src/main/java/com/example/restservice/Product.java
@@ -0,0 +1,14 @@
+package com.example.restservice;
+
+import java.time.LocalDate;
+
+public class Product {
+
+    private String name;
+    private double areaSize;
+    private LocalDate expectedHarvestDate;
+    private double expectedHarvestAmount;
+    private double expectedPrice;
+
+
+}
